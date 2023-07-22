d12a1a3f1c866b8f43aef4744e10875cd34a3c51
Alexey Ozeritskiy <aozeritsky@gmail.com>
2023-07-22 14:36:28 +0200

    Use gtk4

--- solar.c
+++ solar.c
@@ -171,8 +171,8 @@ struct body_ctl
 
 struct App {
     struct body_ctl body_ctls[100];
-    GtkWidget* r[3];
-    GtkWidget* v[3];
+    GtkEntryBuffer* r[3];
+    GtkEntryBuffer* v[3];
     int active_body;
 
     GtkWidget* combo;
@@ -186,8 +186,9 @@ struct App {
  * signal receives a ready-to-be-used cairo_t that is already
  * clipped to only draw the exposed areas of the widget
  */
-static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, struct App* app)
+static void draw_cb(GtkDrawingArea* da, cairo_t *cr, int w1, int h1, void* user_data)
 {
+    struct App* app = user_data;
     int w = 400, i;
     for (i = 0; i < nbodies; ++i)
     {
@@ -207,8 +208,6 @@ static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, struct App* app)
             cairo_show_text(cr, body[i].name);
         }
     }
-
-    return FALSE;
 }
 
 static int get_body(double x, double y, struct App* app) {
@@ -233,32 +232,21 @@ static int get_body(double x, double y, struct App* app) {
     }
 }
 
-/* Handle button press events by either drawing a rectangle
- * or clearing the surface, depending on which button was pressed.
- * The ::button-press signal handler receives a GdkEventButton
- * struct which contains this information.
- */
-static gboolean button_press_event_cb(GtkWidget *widget, GdkEventButton *event, struct App* app)
+static void button_press_event_cb(GtkGestureClick* self, int npress, double x, double y, struct App* app)
 {
-    int argmin = get_body(event->x, event->y, app);
+    int argmin = get_body(x, y, app);
     if (argmin >= 0)
     {
         gtk_combo_box_set_active(GTK_COMBO_BOX(app->combo), argmin);
         app->active_body = argmin;
     }
-
-    /* We've handled the event, stop processing */
-    return TRUE;
 }
 
-/* Handle motion events by continuing to draw if button 1 is
- * still held down. The ::motion-notify signal handler receives
- * a GdkEventMotion struct which contains this information.
- */
-static gboolean motion_notify_event_cb(GtkWidget *widget, GdkEventMotion *event, struct App* app)
+
+static void motion_notify_event_cb(GtkEventControllerMotion* self, double x, double y, struct App* app)
 {
     int i;
-    int argmin = get_body(event->x, event->y, app);
+    int argmin = get_body(x, y, app);
     for (i = 0; i < nbodies; i = i + 1)
     {
         app->body_ctls[i].show_tip = 0;
@@ -267,10 +255,8 @@ static gboolean motion_notify_event_cb(GtkWidget *widget, GdkEventMotion *event,
     {
         app->body_ctls[argmin].show_tip = 1;
     }
-
-    /* We've handled it, stop processing */
-    return TRUE;
 }
+
 
 static void close_window(GtkWidget* widget, struct App* app)
 {
@@ -298,9 +284,9 @@ gboolean redraw_timeout(struct App *app)
     for (j = 0; j < 3; j = j + 1)
     {
         snprintf(buf, sizeof(buf) - 1, "%.16le", body[i].x[j]);
-        gtk_entry_set_text(GTK_ENTRY(app->r[j]), buf);
+        gtk_entry_buffer_set_text(app->r[j], buf, strlen(buf));
         snprintf(buf, sizeof(buf) - 1, "%.16le", body[i].v[j]);
-        gtk_entry_set_text(GTK_ENTRY(app->v[j]), buf);
+        gtk_entry_buffer_set_text(app->v[j], buf, strlen(buf));
     }
 
     int w = 400;
@@ -323,8 +309,7 @@ gboolean redraw_timeout(struct App *app)
     maxx = maxx + 10;
     maxy = maxy + 10;
 
-    /* Now invalidate the affected region of the drawing area. */
-    gtk_widget_queue_draw_area(app->drawing_area, minx, miny, maxx, maxy);
+    gtk_widget_queue_draw(app->drawing_area);
 
     return app->timer_id > 0;
 }
@@ -349,101 +334,96 @@ zoom_scale_changed_cb (GtkGestureZoom *z,
                        gdouble         scale,
                        struct App       *app)
 {
-    Zoom = ZoomInitial * scale;
+    Zoom = ZoomInitial * 1./scale;
     gtk_widget_queue_draw (GTK_WIDGET (app->drawing_area));
 }
 
-static gboolean mouse_scroll (GtkWidget *widget,
-               GdkEvent  *event,
-               struct App       *app)
+static gboolean mouse_scroll(
+    GtkEventControllerScroll* self,
+    double dx, double dy,
+    struct App* app)
 {
-    GdkEventScroll * scroll = (GdkEventScroll*) event;
-
-    if (scroll->direction) {
+    if (dy > 0) {
         Zoom = Zoom * 1.1;
-    } else {
+    } else if (dy < 0) {
         Zoom = Zoom * 0.9;
     }
     gtk_widget_queue_draw (GTK_WIDGET (app->drawing_area));
-
-    return TRUE;
 }
 
 static void activate(GtkApplication *gapp, gpointer user_data)
 {
     struct App* app = user_data;
 
-    int i;
-    GtkWidget * window;
-    GtkWidget *drawing_area;
-    GtkGesture * zoom;
-
+    /*
     GtkCssProvider *css_provider = gtk_css_provider_new();
     gtk_css_provider_load_from_resource(css_provider, "/solar/exampleapp/theme.css");
     gtk_style_context_add_provider_for_screen(
         gdk_screen_get_default(),
         GTK_STYLE_PROVIDER(css_provider),
         GTK_STYLE_PROVIDER_PRIORITY_USER);
+    */
 
-    window = gtk_application_window_new(gapp);
-    gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
-    drawing_area = gtk_drawing_area_new();
+    GtkWidget* window = gtk_application_window_new(gapp);
+    //gtk_window_set_default_size(GTK_WINDOW(window), 1024, 768);
+
+    GtkWidget* drawing_area = gtk_drawing_area_new();
+    gtk_drawing_area_set_content_width(
+        GTK_DRAWING_AREA(drawing_area), 800);
+    gtk_drawing_area_set_content_height(
+        GTK_DRAWING_AREA(drawing_area), 800);
 
     GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    gtk_container_add(GTK_CONTAINER(window), box);
-    //gtk_window_set_child(window, drawing_area);
+    gtk_window_set_child(window, box);
 
-    gtk_box_pack_start(GTK_BOX(box), drawing_area, TRUE, TRUE, 0);
+    gtk_box_append(GTK_BOX(box), drawing_area);
 
     GtkWidget* rbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    gtk_box_pack_start(GTK_BOX(box), rbox, FALSE, FALSE, 0);
+    gtk_box_append(GTK_BOX(box), rbox);
 
     GtkWidget* combo = gtk_combo_box_text_new();
-    for (i=0; i<nbodies; i=i+1) {
+    for (int i = 0; i<nbodies; i=i+1) {
         gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(combo), body[i].name);
     }
     gtk_combo_box_set_active(GTK_COMBO_BOX(combo), 0);
     g_signal_connect(combo, "changed", G_CALLBACK(active_changed), app);
 
-    gtk_box_pack_start(GTK_BOX(rbox), combo, FALSE, FALSE, 0);
-    GtkWidget* x = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), x, FALSE, FALSE, 0);
-    GtkWidget* y = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), y, FALSE, FALSE, 0);
-    GtkWidget* z = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), z, FALSE, FALSE, 0);
-    GtkWidget* vx = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), vx, FALSE, FALSE, 0);
-    GtkWidget* vy = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), vy, FALSE, FALSE, 0);
-    GtkWidget* vz = gtk_entry_new();
-    gtk_box_pack_start(GTK_BOX(rbox), vz, FALSE, FALSE, 0);
-    app->r[0] = x;
-    app->r[1] = y;
-    app->r[2] = z;
-    app->v[0] = vx;
-    app->v[1] = vy;
-    app->v[2] = vz;
+    gtk_box_append(GTK_BOX(rbox), combo);
+    for (int i = 0; i < 3; i++) {
+        GtkWidget* x = gtk_entry_new();
+        gtk_box_append(GTK_BOX(rbox), x);
+        app->r[i] = gtk_entry_get_buffer(GTK_ENTRY(x));
+    }
+    for (int i = 0; i < 3; i++) {
+        GtkWidget* vx = gtk_entry_new();
+        gtk_box_append(GTK_BOX(rbox), vx);
+        app->v[i] = gtk_entry_get_buffer(GTK_ENTRY(vx));
+    }
     app->combo = combo;
 
-    g_signal_connect(drawing_area, "draw", G_CALLBACK(draw_cb), app);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), draw_cb, app, NULL);
 
+    GtkEventController* motion = gtk_event_controller_motion_new();
+    g_signal_connect(motion, "motion", G_CALLBACK(motion_notify_event_cb), app);
+    gtk_event_controller_set_propagation_phase(motion, GTK_PHASE_CAPTURE);
+    gtk_widget_add_controller(drawing_area, motion);
 
-    gtk_widget_set_events(
-        drawing_area,
-        gtk_widget_get_events(drawing_area)
-        | GDK_BUTTON_PRESS_MASK | GDK_SCROLL_MASK | GDK_POINTER_MOTION_MASK);
+    GtkGesture* gclick = gtk_gesture_click_new();
+    g_signal_connect(gclick, "pressed", G_CALLBACK(button_press_event_cb), app);
+    gtk_event_controller_set_propagation_phase(
+        GTK_EVENT_CONTROLLER(gclick), GTK_PHASE_CAPTURE);
+    gtk_widget_add_controller(drawing_area, GTK_EVENT_CONTROLLER(gclick));
 
-    // moved to GtkEventControllerMotion
-    g_signal_connect(drawing_area, "motion-notify-event", G_CALLBACK(motion_notify_event_cb), app);
+    GtkEventController* scroll = gtk_event_controller_scroll_new(
+        GTK_EVENT_CONTROLLER_SCROLL_VERTICAL);
+    g_signal_connect(scroll, "scroll", G_CALLBACK(mouse_scroll), app);
+    gtk_event_controller_set_propagation_phase(scroll, GTK_PHASE_CAPTURE);
+    gtk_widget_add_controller(drawing_area, scroll);
 
-    // moved to GtkGestureClick
-    g_signal_connect(drawing_area, "button-press-event", G_CALLBACK(button_press_event_cb), app);
-
-    // moved to GtkEventControllerScroll
-    g_signal_connect(drawing_area, "scroll-event", G_CALLBACK(mouse_scroll), app);
-
-    zoom = gtk_gesture_zoom_new (drawing_area);
+    GtkGesture* zoom = gtk_gesture_zoom_new();
+    gtk_event_controller_set_propagation_phase(
+        GTK_EVENT_CONTROLLER(zoom), GTK_PHASE_CAPTURE);
+    gtk_widget_add_controller(drawing_area, GTK_EVENT_CONTROLLER(zoom));
 
     g_signal_connect(zoom, "begin", G_CALLBACK(zoom_begin_cb), app);
     g_signal_connect(zoom, "scale-changed", G_CALLBACK(zoom_scale_changed_cb), app);
@@ -453,7 +433,7 @@ static void activate(GtkApplication *gapp, gpointer user_data)
     app->drawing_area = drawing_area;
     app->timer_id = g_timeout_add(100, (GSourceFunc)redraw_timeout, app);
 
-    gtk_widget_show_all(window);
+    gtk_widget_set_visible(window, TRUE);
 }
 
 int main(int argc, char **argv)
